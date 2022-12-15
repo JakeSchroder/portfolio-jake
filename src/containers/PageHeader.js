4d01e840e3bac770e122a6736d512f9e4aeafb9f
JakeSchroder <jake.schroder@gmail.com>
2022-12-15 09:50:32 -0600

    Add dark mode

--- src/containers/PageHeader.js
+++ src/containers/PageHeader.js
@@ -10,28 +10,32 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import { Grid } from '@mui/material';
 import { positions } from '@mui/system';
 import MessageFormComponent from '../components/MessageForm';
+import NightsStayIcon from '@mui/icons-material/NightsStay';
 
-export default function PageHeader() {
-  return (
-        <Toolbar>
-            <Box display='flex' flexGrow={1}>
-                <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                >
-                    <MenuIcon />
+export default function PageHeader({mode, setMode}) {
+    const handleClick = () =>{
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+    }
+    return (
+            <Toolbar>
+                <Box display='flex' flexGrow={1}>
+                    <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Box>
+
+                <Button color="inherit">About</Button>
+                <Button color="inherit">Projects</Button>
+                <MessageFormComponent/>
+                <Button variant="contained" color="secondary" sx={{borderRadius: 28}} endIcon={<CallMadeIcon/>}>Resume</Button>
+                <IconButton size="large" onClick={handleClick}>
+                    {mode === 'dark' ? <LightModeIcon fontSize='inherit'/> : <NightsStayIcon fontSize='inherit'/>}
                 </IconButton>
-            </Box>
-
-            <Button color="inherit">About</Button>
-            <Button color="inherit">Projects</Button>
-            <MessageFormComponent/>
-            <Button variant="contained" color="secondary" sx={{borderRadius: 28}} endIcon={<CallMadeIcon/>}>Resume</Button>
-            <IconButton size="large">
-                <LightModeIcon fontSize='inherit'/>
-            </IconButton>
-        </Toolbar>
-  );
+            </Toolbar>
+    );
 }
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,18 +1,31 @@
-import * as React from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 import PageHeaderContainer from "../containers/PageHeader";
 import PageFooterContainer from '../containers/PageFooter';
 import ProjectPageBodyContainer from '../containers/ProjectPageBody';
 import AboutPageBodyContainer from '../containers/AboutPageBody';
 import { Box, Grid, AppBar} from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
 
 const HomePage = () => {
+  const [mode, setMode] = useState('light');
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <div className="HomePage">  
+    <div className="HomePage">
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
         <AppBar className='HomePage-header' color="transparent" elevation={0} position='absolute'>
-            <PageHeaderContainer/>
+            <PageHeaderContainer mode={mode} setMode={setMode}/>
         </AppBar>
         <AboutPageBodyContainer/>
         <PageFooterContainer/>
+      </ThemeProvider>  
     </div>
   );
 }
