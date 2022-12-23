c09b0710b6d067fe1f182da6077517f6d764e881
JakeSchroder <jake.schroder@gmail.com>
2022-12-23 14:58:51 -0600

    Add qol features to tabs and cards

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,29 +1,36 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Grid, Toolbar, Button, IconButton} from '@mui/material';
+import MessageFormComponent from '../components/MessageForm';
 import CallMadeIcon from '@mui/icons-material/CallMade';
 import LightModeIcon from '@mui/icons-material/LightMode';
-import { Grid, Toolbar, Button} from '@mui/material';
-import MessageFormComponent from '../components/MessageForm';
 import NightsStayIcon from '@mui/icons-material/NightsStay';
-import { ReactComponent as Logo } from "../logo.svg";
-import { useRef } from 'react';
 
 export default function Header({mode, setMode}) {
-    const handleClick = () =>{
+    const goToAbout = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+    const goToProjects = () => {
+        window.scrollTo({
+            top: window.innerHeight*.85,
+            behavior: "smooth",
+        });
+    };
+    const toggleDarkMode = () =>{
         setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
     }
 
     return (
         <Grid container justifyContent='center' rowGap={0}>
             <Toolbar >
-                <Button color="inherit" >About</Button>
-                <Button color="inherit">Projects</Button>
+                <Button color="inherit" onClick={goToAbout}>About</Button>
+                <Button color="inherit" onClick={goToProjects}>Projects</Button>
                 <MessageFormComponent/>
                 <Button variant="contained" color="secondary" href='/Resume-FullStack.pdf' target="_blank" sx={{borderRadius: 28}} endIcon={<CallMadeIcon/>}>
                     Resume
                 </Button>
-                <IconButton size="large" onClick={handleClick} >
+                <IconButton size="large" onClick={toggleDarkMode} >
                     {mode === 'dark' ? <LightModeIcon fontSize='inherit'/> : <NightsStayIcon fontSize='inherit'/>}
                 </IconButton>
             </Toolbar>
