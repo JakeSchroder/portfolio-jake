575c1a9ff8300bd730f9c2e71d0a070986da9c16
JakeSchroder <jake.schroder@gmail.com>
2022-12-26 16:46:57 -0600

    Add transformations to page elements

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -7,7 +7,9 @@ export default function Footer() {
     return (
         <Grid container justifyContent='center' sx={{backgroundColor:'#8E24AA', height:'160px'}}>
             <Grid item xs={12} display="flex" justifyContent="center" alignItems="center">
-                <Typography sx={{fontFamily:'solano-gothic-pro-mvb, sans-serif', fontWeight:'500', fontStyle: 'normal'}}>Jake.Schroder@gmail.com</Typography>
+                <Typography sx={{fontFamily:'solano-gothic-pro-mvb, sans-serif', fontWeight:'500', fontStyle: 'normal'}}>
+                    <b>Jake.Schroder@gmail.com</b>
+                </Typography>
             </Grid>
             <Grid item xs={12} display="flex" justifyContent="center" alignItems="center" height={30}>
                 <IconButton size='large' target='_blank' href='https://github.com/JakeSchroder'>
@@ -18,7 +20,9 @@ export default function Footer() {
                 </IconButton>
             </Grid>
             <Grid item xs={12} display="flex" justifyContent="center" alignItems="center" height={20}>
-                <Typography sx={{fontFamily:'solano-gothic-pro-mvb, sans-serif', fontWeight:'500', fontStyle: 'normal'}}>Jake Schroder, 2022. All Rights Reserved</Typography>
+                <Typography sx={{fontFamily:'solano-gothic-pro-mvb, sans-serif', fontWeight:'500', fontStyle: 'normal'}}>
+                    <b>Jake Schroder, 2022. All Rights Reserved</b>
+                </Typography>
             </Grid>
 
         </Grid>
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,10 +1,23 @@
-import { Grid, Toolbar, Button, IconButton} from '@mui/material';
+import React, { useState, useEffect, useCallback } from "react";
+import { Grid, Toolbar, Button, IconButton, keyframes} from '@mui/material';
 import MessageFormComponent from '../components/MessageForm';
 import CallMadeIcon from '@mui/icons-material/CallMade';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import NightsStayIcon from '@mui/icons-material/NightsStay';
 
 export default function Header({mode, setMode}) {
+    const [clicked, setClicked] = useState(false);
+    const rotateScaleUp = keyframes`
+        0% {
+            transform: scale(1) rotateZ(0);
+        }
+        50% {
+            transform: scale(2) rotateZ(180deg);
+        }
+        100% {
+            transform: scale(1) rotateZ(360deg);
+        }`;
+
     const goToAbout = () => {
         window.scrollTo({
             top: 0,
@@ -18,7 +31,11 @@ export default function Header({mode, setMode}) {
         });
     };
     const toggleDarkMode = () =>{
+        setClicked(true)
         setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+        setTimeout(() => {
+            setClicked(false)
+        }, 500)
     }
 
     return (
@@ -30,8 +47,8 @@ export default function Header({mode, setMode}) {
                 <Button variant="contained" color="secondary" href='/Resume-FullStack.pdf' target="_blank" sx={{borderRadius: 28}} endIcon={<CallMadeIcon/>}>
                     Resume
                 </Button>
-                <IconButton size="large" onClick={toggleDarkMode} >
-                    {mode === 'dark' ? <LightModeIcon fontSize='inherit'/> : <NightsStayIcon fontSize='inherit'/>}
+                <IconButton size="large" onClick={toggleDarkMode} sx={{ animation: clicked ? `${rotateScaleUp} .5s linear both` : ''}}>
+                    {mode === 'dark' ? <LightModeIcon fontSize='inherit' /> : <NightsStayIcon fontSize='inherit'/>}
                 </IconButton>
             </Toolbar>
         </Grid>
