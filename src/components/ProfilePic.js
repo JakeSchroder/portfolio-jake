3bce8d5c3fe19ae889ac0128bac0ddb4637b7036
JakeSchroder <jake.schroder@gmail.com>
2023-01-26 21:29:58 -0600

    Prepare for mobile view

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,34 +1,68 @@
-import { Box, Grid, keyframes } from "@mui/material"
+import { Box, Grid, keyframes } from "@mui/material";
 
-export default function ProfilePic({theme}){
-    const blob= keyframes `
+export default function ProfilePic({ theme, isMobile }) {
+  const blob = keyframes`
     10% { border-radius: 33% 67% 50% 50% / 43% 39% 61% 57%; }
-      
     20% { border-radius: 51% 49% 31% 69% / 65% 39% 61% 35%; }
-      
     30% { border-radius: 51% 49% 56% 44% / 45% 39% 61% 55%; }
-      
     40% { border-radius: 66% 34% 33% 67% / 48% 71% 39% 52%; }
-      
     50% { border-radius: 46% 54% 33% 67% / 48% 30% 70% 52%; }
-      
     60% { border-radius: 46% 54% 56% 44% / 48% 30% 70% 52%; }
-      
     70% { border-radius: 46% 54% 56% 44% / 65% 53% 47% 35%; }
-      
     80% { border-radius: 67% 33% 56% 44% / 37% 53% 47% 63%; }
-      
-    90% { border-radius: 46% 54% 32% 68% / 37% 53% 47% 63%; }` 
-    const blobAnim = `${blob} 60s linear infinite`;
-
-    return(
-        <Grid item xs={5} display="flex" justifyContent="center" alignItems="center">
-            <Box display='flex' justifyContent='center' alignItems="center" width={500} height={600} sx={{backgroundColor: theme.palette.secondary.light, animation: blobAnim, borderRadius: '50%' }}>
-                <Box display='flex' position='relative' justifyContent='center' alignItems="center" width={600} height={450} left={30} top={20} sx={{overflow:'hidden' ,backgroundColor: theme.palette.primary.contrastText, animation: blobAnim, borderRadius: '50%' }}>
-                    <img alt="Jake Schroder" width={500} height={500} src="/images/jake-profile.png"/>
-                </Box>
-            </Box>
-        </Grid>
-
-    )
+    90% { border-radius: 46% 54% 32% 68% / 37% 53% 47% 63%; }
+  `;
+  const blobAnim = `${blob} 60s linear infinite`;
+  const profileImageSize = isMobile
+    ? { width: 200, height: 200 }
+    : { width: 500, height: 500 };
+  const largeCircleSize = isMobile
+    ? { width: 200, height: 300 }
+    : { width: 500, height: 600 };
+  const smallCircleSize = isMobile
+    ? { width: 300, height: 225 }
+    : { width: 600, height: 450 };
+  return (
+    <Grid
+      item
+      xs={5}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        {...largeCircleSize}
+        sx={{
+          backgroundColor: theme.palette.secondary.light,
+          animation: blobAnim,
+          borderRadius: "50%",
+        }}
+      >
+        <Box
+          display="flex"
+          position="relative"
+          justifyContent="center"
+          alignItems="center"
+          {...smallCircleSize}
+          left={30}
+          top={20}
+          sx={{
+            overflow: "hidden",
+            backgroundColor: theme.palette.primary.contrastText,
+            animation: blobAnim,
+            borderRadius: "50%",
+          }}
+        >
+          <img
+            alt="Jake Schroder"
+            {...profileImageSize}
+            src="/images/jake-profile.png"
+          />
+        </Box>
+      </Box>
+    </Grid>
+  );
 }
--- src/containers/AboutBody.js
+++ src/containers/AboutBody.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { Grid} from '@mui/material';
+import { Grid } from "@mui/material";
 import ProfilePic from "../components/ProfilePic";
 import TechIcons from "../components/TechIcons";
 import AboutBodyText from "../components/AboutBodyText";
 
-export default function AboutBody({theme}) {
-
-
-    return (
-        <Grid id='aboutBody' container elevation={0} justifyContent="space-between" display='flex' sx={{paddingTop:'3%', paddingRight:10}}>
-            <ProfilePic theme={theme}/>
-            <TechIcons theme={theme}/>
-            <AboutBodyText theme={theme}/>
-        </Grid>
-    );
-  }
+export default function AboutBody({ theme, isMobile }) {
+  return (
+    <Grid
+      id="aboutBody"
+      container
+      elevation={0}
+      justifyContent="space-between"
+      display="flex"
+      sx={{ paddingTop: "3%", paddingRight: 10 }}
+    >
+      <ProfilePic theme={theme} isMobile={isMobile} />
+      {!isMobile && <TechIcons theme={theme} />}
+      <AboutBodyText theme={theme} isMobile={isMobile} />
+    </Grid>
+  );
+}
